7ec7545d80d0443f9fecf0e62949c6437c143627
Raul Alejandro - Gasmontsoft <raul_rodriguez@flotal.com.mx>
2025-12-03 17:02:08 -0600

    Corregir titulo de llanta seleccionada

--- app/src/main/java/com/rfz/appflotal/presentation/ui/assembly/screen/AssemblyTireScreen.kt
+++ app/src/main/java/com/rfz/appflotal/presentation/ui/assembly/screen/AssemblyTireScreen.kt
@@ -237,17 +237,19 @@ fun AssemblyTireView(
                                 Text(text = stringResource(R.string.seleccione_una_llanta))
                             }
 
+                            if (uiState.currentTire != null){
+                                Text(
+                                    text = stringResource(R.string.llanta_seleccionada),
+                                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.SemiBold),
+                                )
+                            }
+
                             AnimatedVisibility(
                                 visible = uiState.currentTire != null,
                                 enter = expandVertically() + fadeIn(),
                                 exit = shrinkVertically() + fadeOut(),
                                 modifier = Modifier.padding(top = 16.dp)
                             ) {
-                                Text(
-                                    text = stringResource(R.string.llanta_seleccionada),
-                                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.SemiBold),
-                                )
-
                                 TireInfoCard(
                                     tire = uiState.currentTire,
                                     modifier.width(240.dp)
