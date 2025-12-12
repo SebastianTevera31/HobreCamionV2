2f3d343887d22ddd4e2a1853eebfbe4a3c39c128
Raul Alejandro - Gasmontsoft <raul_rodriguez@flotal.com.mx>
2025-12-12 16:55:24 -0600

    Corregir versionCode - Flotal v3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.rfz.appflotal"
         minSdk = 28
         targetSdk = 35
-        versionCode = 9
+        versionCode = 10
         versionName = "3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
