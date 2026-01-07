ab46483068f833cfcda3b33f732c34c2e2dbb8c1
Raul Alejandro - Gasmontsoft <raul_rodriguez@flotal.com.mx>
2026-01-07 17:06:24 -0600

    Agregar barra superior de rendimiento

--- app/src/main/java/com/rfz/appflotal/presentation/ui/reportes/Rendimiento.kt
+++ app/src/main/java/com/rfz/appflotal/presentation/ui/reportes/Rendimiento.kt
@@ -1,4 +1,34 @@
 package com.rfz.appflotal.presentation.ui.reportes
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rfz.appflotal.presentation.commons.SimpleTopBar
+import com.rfz.appflotal.presentation.theme.HombreCamionTheme
 
+@Composable
+fun RendimientoView(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            SimpleTopBar(
+                title = "Reportes",
+                onBack = {},
+                showBackButton = true,
+                subTitle = ""
+            )
+        }
+    ) { innerPadding ->
+
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun RendimientoScreenPreview() {
+    HombreCamionTheme {
+        RendimientoView()
+    }
+}
