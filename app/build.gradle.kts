8f353da8e34ca95d7b47cffb0b492591aee65c75
Raul Alejandro - Gasmontsoft <raul_rodriguez@flotal.com.mx>
2026-04-30 16:39:53 -0600

    Agregar traducciones y biblioteca de geocoder

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -153,6 +153,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
+    // Localizacion
+    implementation("com.google.android.gms:play-services-location:21.2.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+
     // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
