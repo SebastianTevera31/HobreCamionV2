375f3e51db70db48e834986fd7af3e1d4a1fa418
Raul Alejandro - Gasmontsoft <raul_rodriguez@flotal.com.mx>
2026-05-17 14:08:46 -0600

    Subir versionCode a 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.rfz.appflotal"
         minSdk = 28
         targetSdk = 35
-        versionCode = 20
-        versionName = "3.1"
+        versionCode = 21
+        versionName = "3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
