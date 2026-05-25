37b08e266c51d4d89fb9082f7e02e9690dfe9ee6
Raul Alejandro - Gasmontsoft <raul_rodriguez@flotal.com.mx>
2026-05-25 12:58:29 -0600

    Cambiar User Agent a Android 6.0

--- app/src/main/java/com/rfz/appflotal/presentation/ui/vialstatus/view/VialStatusScreen.kt
+++ app/src/main/java/com/rfz/appflotal/presentation/ui/vialstatus/view/VialStatusScreen.kt
@@ -461,10 +461,9 @@ fun VialStatusWebView(
                         displayZoomControls = false
                         setSupportZoom(true)
 
-                        userAgentString =
-                            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
-                                    "AppleWebKit/537.36 (KHTML, like Gecko) " +
-                                    "Chrome/120.0.0.0 Safari/537.36"
+                        userAgentString = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) " +
+                                    "AppleWebKit/537.36 (KHTML, like Gecko)" +
+                                " Chrome/148.0.0.0 Mobile Safari/537.36"
                     }
 
                     loadUrl(url)
