99d88652521edc2b01ae1b9418c858b2f1b6c7b5
Raul Alejandro - Gasmontsoft <raul_rodriguez@flotal.com.mx>
2026-05-26 12:48:25 -0600

    Agregar vial_status

--- app/src/main/java/com/rfz/appflotal/presentation/ui/forums/navigation/ForumNavigation.kt
+++ app/src/main/java/com/rfz/appflotal/presentation/ui/forums/navigation/ForumNavigation.kt
@@ -52,47 +52,6 @@ import com.rfz.appflotal.presentation.ui.forums.viewmodel.CameraUiState
 import com.rfz.appflotal.presentation.ui.forums.viewmodel.ForumScreenType
 import com.rfz.appflotal.presentation.ui.forums.viewmodel.ForumViewModel
 import com.rfz.appflotal.presentation.ui.utils.LoadState
-import kotlinx.serialization.Serializable
-
-@Serializable
-object ForumsGraph
-
-@Serializable
-object ForumRooms
-
-@Serializable
-data class RoomTopics(
-    val roomId: String,
-    val roomTitle: String,
-)
-
-@Serializable
-data class TopicDiscussion(
-    val roomId: String,
-    val topicId: String,
-    val topicTitle: String,
-    val selectedComment: String = "0"
-)
-
-@Serializable
-data class ReportContent(
-    val id: Int,
-    val isComment: Boolean
-)
-
-@Serializable
-data class NewTopicNav(
-    val roomId: String,
-    val roomTitle: String
-)
-
-@Serializable
-data class NewCommentNav(
-    val commentId: Int
-)
-
-@Serializable
-object SavedCommentsNav
 
 fun NavGraphBuilder.forumsGraph(
     navController: NavHostController
---
+++ app/src/main/java/com/rfz/appflotal/presentation/ui/forums/navigation/ForumRoutes.kt
@@ -0,0 +1,43 @@
+package com.rfz.appflotal.presentation.ui.forums.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object ForumsGraph
+
+@Serializable
+object ForumRooms
+
+@Serializable
+data class RoomTopics(
+    val roomId: String,
+    val roomTitle: String,
+)
+
+@Serializable
+data class TopicDiscussion(
+    val roomId: String,
+    val topicId: String,
+    val topicTitle: String,
+    val selectedComment: String = "0"
+)
+
+@Serializable
+data class ReportContent(
+    val id: Int,
+    val isComment: Boolean
+)
+
+@Serializable
+data class NewTopicNav(
+    val roomId: String,
+    val roomTitle: String
+)
+
+@Serializable
+data class NewCommentNav(
+    val commentId: Int
+)
+
+@Serializable
+object SavedCommentsNav
